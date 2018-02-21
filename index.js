37b7162802ef96062dd06c1d964f3b1d66bbc3ba
Samuel Chase <samuelbchase@gmail.com>
2018-02-21 13:34:52 -0800

    Initial version of working chat app
    This heavily utilizes the teaching functions from SocketIO, and must be further modified to suit our needs

---
+++ index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var path = require('path');
+var http = require('http').Server(app);
+
+var io = require('socket.io')(http);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
