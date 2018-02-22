0ea2f7bfd425498dd127cf0e0d3bd27b8a18c2e9
Samuel Chase <samuelbchase@gmail.com>
2018-02-22 00:07:09 -0800

    Added different panes for each friend

--- index.js
+++ index.js
@@ -9,24 +9,6 @@ var io = require('socket.io')(http);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-var con = mysql.createConnection({
-    host: "hardworlder.com",
-    user: "readOnlyWhisper",
-    password: "Einherjar255!",
-    database: "whisperio"
-});
-
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    var sql = "SELECT * FROM Friends where Host = " + "'Sam';";
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("Result: " + result);
-    });
-});
-
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
@@ -35,28 +17,64 @@ app.get('/login', function(req, res){
     res.sendFile(__dirname + '/login.html');
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-});
+var sockets = [];
+var names = [];
 
 io.on('connection', function(socket){
+    console.log('a user connected');
     socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
-    });
-});
 
-io.on('connection', function(socket){
-    console.log('a user connected');
+        var indexOfSeparator = msg.indexOf('-');
+        var userSentTo = msg.slice(0,indexOfSeparator);
+        var message = msg.slice(indexOfSeparator+1);
+        console.log("----------------------------");
+        console.log('message: ' + message);
+        console.log('Was set to: ' + userSentTo);
+        var name = "Unknown";
+        for(var i = 0; i < sockets.length;i++)
+        {
+            if(sockets[i] === socket)
+            {
+                name = names[i];
+            }
+        }
+        console.log('By: ' + name);
+        console.log("----------------------------");
+
+        io.emit('chat message', message);
+    });
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
+    socket.on('userNameSend', function(userName){
+        sockets.push(socket);
+        names.push(userName);
+        var con = mysql.createConnection({
+            host: "hardworlder.com",
+            user: "readOnlyWhisper",
+            password: "Einherjar255!",
+            database: "whisperio"
+        });
+
+        con.connect(function(err) {
+            if (err) throw err;
+            //console.log("Connected!");
+            var sql = "SELECT * FROM Friends where Host = '" + userName + "';";
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+                io.emit('FriendsList',result);
+                console.log("Friends list sent: " + result);
+            });
+            con.end()
+        });
+
+    });
+
 });
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
-    });
-});
+
+
+
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
