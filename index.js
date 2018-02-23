9bf0b8a964de030b03282d12b8e8942b78587b04
Samuel Chase <samuelbchase@gmail.com>
2018-02-22 20:54:47 -0800

    Updated CSS

--- index.js
+++ index.js
@@ -22,11 +22,9 @@ var names = [];
 
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
-
         var indexOfSeparator = msg.indexOf('-');
         var userSentTo = msg.slice(0,indexOfSeparator);
         var message = msg.slice(indexOfSeparator+1);
-        console.log("----------------------------");
         console.log('message: ' + message);
         console.log('Was set to: ' + userSentTo);
         var name = "Unknown";
@@ -35,21 +33,11 @@ io.on('connection', function(socket){
             if(sockets[i] === socket)
             {
                 name = names[i];
-                console.log(name + ' connected');
             }
         }
         console.log('By: ' + name);
         console.log("----------------------------");
-        var socketToSendTo;
-        for(i = 0; i < names.length;i++)
-        {
-            if(names[i] === userSentTo)
-            {
-                socketToSendTo = sockets[i];
-                console.log("Sending to socketID " + names[i]);
-            }
-        }
-        socket.broadcast.to(socketToSendTo).emit('chat message',message);
+        socket.broadcast.to(userSentTo).emit('chat message',message);
     });
     socket.on('disconnect', function(){
         console.log('user disconnected');
@@ -57,7 +45,8 @@ io.on('connection', function(socket){
     socket.on('userNameSend', function(userName){
         sockets.push(socket);
         names.push(userName);
-        console.log("New User Connected: " + userName);
+        socket.id = userName;
+        console.log("New User Connected: " + socket.id);
             var con = mysql.createConnection({
                 host: "hardworlder.com",
                 user: "readOnlyWhisper",
@@ -67,7 +56,8 @@ io.on('connection', function(socket){
             var sql = "SELECT * FROM Friends where Host = '" + userName + "';";
             con.query(sql, function (err, result) {
                 if (err) throw err;
-                console.log("Broadcasting friends to " + userName);
+                //console.log("Broadcasting friends to " + userName);
+                console.log("----------------------------");
                 socket.emit('FriendsList',result);
                 //console.log("Friends list sent: " + result);
             });
