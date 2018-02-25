14dfe978cd84af45010c9c332809db50b3a5132d
unknown <gdaswegan@gmail.com>
2018-02-24 20:32:34 -0800

    Finished the Add Friend button

--- index.js
+++ index.js
@@ -19,6 +19,44 @@ app.get('/login', function(req, res){
 
 var sockets = [];
 var names = [];
+var read;
+var write;
+
+var fs = require("fs");
+var host;
+var database;
+var readUN;
+var readPW;
+var writeUN;
+var writePW;
+
+//use this for opening a file for the read and write passwords for the DB	
+//PLEASE DON'T MESS WITH THIS FUNCTION OR .info.txt! IT WILL SCREW UP THE DATABASE QUERYS
+fs.readFile('.info.txt', 'utf8', function(err, contents){
+	var index = contents.indexOf('|');
+	var old = 0;
+	host = contents.slice(old, index);
+	
+	old = index + 3;
+	index = contents.indexOf('|', old);
+	database = contents.slice(old, index);
+	
+	old = index + 3;
+	index = contents.indexOf('|', old);
+	readUN = contents.slice(old, index);
+	
+	old = index + 3;
+	index = contents.indexOf('|', old);	
+	readPW = contents.slice(old, index);
+	
+	old = index + 3;
+	index = contents.indexOf('|', old);
+	writeUN = contents.slice(old, index);
+	
+	old = index + 3;
+	index = contents.indexOf('|', old);
+	writePW = contents.slice(old);
+});
 
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
@@ -54,22 +92,64 @@ io.on('connection', function(socket){
         names.push(userName);
         socket.id = userName;
         console.log("New User Connected: " + socket.id);
-        var con = mysql.createConnection({
-            host: "hardworlder.com",
-            user: "readOnlyWhisper",
-            password: "Einherjar255!",
-            database: "whisperio"
-        });
+		read = mysql.createConnection({
+			host: host,
+			user: readUN,
+			password: readPW,
+			database: database,
+		});
         var sql = "SELECT * FROM Friends where Host = '" + userName + "';";
-        con.query(sql, function (err, result) {
+        read.query(sql, function (err, result) {
             if (err) throw err;
             //console.log("Broadcasting friends to " + userName);
             console.log("----------------------------");
             socket.emit('FriendsList',result);
             //console.log("Friends list sent: " + result);
         });
-        con.end()
+		read.end();
     });
+	
+	//Add Friend button is pushed; called by currentUser adding friendToAdd
+	socket.on('addFriend', function (currentUser, friendToAdd) {
+		console.log("Adding " + friendToAdd + " for " + currentUser + " as a friend");
+		read = mysql.createConnection({
+			host: host,
+			user: readUN,
+			password: readPW,
+			database: database,
+		});;
+		
+		//check to see if the friend relationship already exists
+		var sql = "SELECT * FROM Friends WHERE Host = \"" + currentUser + "\" AND Receiver = \"" + friendToAdd + "\";"
+		read.query(sql, function(err, result) {
+			if (err) throw err;
+			if (result.length == 0) // if the friend relationship doesn't exist
+			{
+				console.log("New friend!");
+				
+				sql = "SELECT * FROM User WHERE username = \"" + friendToAdd + "\";"
+				read.query(sql, function(err, result) {	
+					if (err) throw err;
+					if (result.length > 0)	// make sure that the friend you're adding actually exists
+					{
+						write = mysql.createConnection({
+							host: host,
+							user: writeUN,
+							password: writePW,
+							database: database,
+						});;
+						sql = "INSERT INTO Friends (Host, Receiver) VALUES ('" + currentUser + "', '" + friendToAdd + "');"
+						write.query(sql, function(err, result) {
+							if (err) throw err;
+						});
+					}
+					else
+						console.log("User does not exist!");
+				});
+			}
+			else console.log("Friend already exists");
+		});
+	});
 });
 
 http.listen(3000, function(){
