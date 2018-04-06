59b505ef4bf457c4b983e7368a96b65eedf9ea1d
Samuel Chase <samuelbchase@gmail.com>
2018-04-05 20:04:54 -0700

    Added basic test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,33 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+// test/test.js
+
+var expect = require('chai').expect;
+var io     = require('socket.io-client');
+var app = require('../index.js');
+
+var socketUrl = 'http://localhost:3000/main';
+
+var options = {
+    transports: ['websocket'],
+    'force new connection': true
+};
+
+var room = 'lobby';
+
+describe('Sockets', function () {
+    var client1, client2, client3;
+
+    // ... test.js
+
+    it('should send and receive a message', function (done) {
+        // Set up client1 connection
+        client1 = io.connect(socketUrl, options);
+        client2 = io.connect(socketUrl, options);
+
+        // Set up event listener.  This is the actual test we're running
+        client1.emit('userNameSend', "Griffin");
+        client2.emit('userNameSend', "Sam");
+        client2.emit('userLogin', "Griffin");
+        client2.emit('userLogin', "Sam");
+        done();
     });
-  });
 });
