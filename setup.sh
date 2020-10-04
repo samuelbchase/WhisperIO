0d0eaf6447c3d13e8724b46b97349da2db581869
sam <samuelbchase@gmail.com>
2020-10-03 22:41:09 -0700

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Please enter the server's IP address, followed by [ENTER]:"
+
+read ip
+
+echo "Please enter the google signin client ID, followed by [ENTER]:"
+
+read clientId
+
+#update IP address
+#sed -i "s/var ip =.*/var ip = \"$ip\";/g" index.html
+#sed -i "s/var ip =.*/var ip = \"$ip\";/g" login.html
+#update google client ID
+sed -i "s/.*google-signin-client_id.*/\t<meta name=\"google-signin-client_id\" content=\"$clientId\">/g" index.html
